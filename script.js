8bb64de6b098622bcceaedeb66a483750305dc89
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-07-20 23:51:35 -0300

    Novas fotos e script.
    Tinha algumas imagens que estavam com tamanho menor, e assim deixando o conteúdo menos responsivo. E as imagens não estavam se ajustando de acordo com o horário pois invés do id ser declarado em img, tinha sido declarado em section.

--- script.js
+++ script.js
@@ -19,19 +19,21 @@ hoje.innerHTML = `${horas}:${minutos}, ${dia} de ${mes} de ${ano}`
 
 function carregar() {
     if (horas >= 0 && horas <= 5) {
-       imagem.src = 'imagens/noite-pc.png' 
+       imagem.src = 'imagens/lua.png' 
        document.body.style.background = '#40019A'
     } else if (horas <= 11) {
-        imagem.src = 'imagens/sol-pc'
+        imagem.src = 'imagens/sol.png'
         document.body.style.background = '#FFED4B'
     } else if (horas <= 17) {
-        imagem.src = 'imagens/dia-sol-pc'
+        imagem.src = 'imagens/sol-nuvens.png'
         document.body.style.background = '#7FE0E9'
     } else if (horas <= 24) {
-        imagem.src = "imagens/noite-pc.png"
+        imagem.src = "imagens/lua.png"
         document.body.style.background = '#40019A'
     } else {
         imagem.remove();
     }
+    console.log('imagens/lua.png' + imagem.src); // Log para verificar o caminho da imagem
+
     
 }
